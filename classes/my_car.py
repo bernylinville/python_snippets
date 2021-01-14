de231dc200864bd0d2fb61d36de28202bc6b1a64
bernylinville <bernylinville@devopsthink.org>
2021-01-14 17:52:40 +0800

    first commit

---
+++ classes/my_car.py
@@ -0,0 +1,10 @@
+from car import Car
+
+my_new_car = Car('Audi', 'a4', 2019)
+print(my_new_car.get_descriptive_name())
+
+my_new_car.odometer_reading = 23
+my_new_car.read_odometer()
+
+my_new_car.update_odometer(22)
+my_new_car.read_odometer()
