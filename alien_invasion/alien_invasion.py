c2f0d7ca8b9cefa3278ad200c6de02fbec1afadb
bernylinville <bernylinville@devopsthink.org>
2021-01-19 18:33:06 +0800

    fix

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -6,7 +6,7 @@ class AlienInvasion:
     """Overall class to manage game assets and behavior."""
 
     def __init__(self):
-       """Initialize the game, and create game resources."""
+        """Initialize the game, and create game resources."""
         pygame.init()
 
         self.screen = pygame.display.set_mode((1200, 800))
