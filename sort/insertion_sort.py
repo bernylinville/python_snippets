5913c9e916b22ead3c11f9f4ace066c0b68edad6
bernylinville <bernylinville@devopsthink.org>
2021-03-04 10:47:09 +0800

    insertion_sort

---
+++ sort/insertion_sort.py
@@ -0,0 +1,16 @@
+# coding:utf-8
+
+import random
+
+def insertion_sort(nums):
+    indexs = len(nums)
+    for i in range(1, indexs):
+        while i > 0 and nums[i - 1] > nums[i]:
+            nums[i - 1], nums[i] = nums[i], nums[i - 1]
+            i -= 1
+
+    return nums
+
+nums = random.sample(list(range(20)), 5)
+print(nums)
+print(insertion_sort(nums))
