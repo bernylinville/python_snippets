11ca757444a8af41d5f7c5ff24f8d4f69b0c2b75
bernylinville <bernylinville@devopsthink.org>
2021-03-04 10:21:32 +0800

    selection_sort

---
+++ sort/selection_sort.py
@@ -0,0 +1,16 @@
+# coding:utf-8
+
+import random
+
+def selection_sort(nums):
+    indexs = len(nums)
+    for i in range(indexs):
+        for j in range(i, indexs):
+            if nums[i] > nums[j]:
+                nums[i], nums[j] = nums[j], nums[i]
+
+    return nums
+
+nums = random.sample(list(range(20)), 5)
+print(nums)
+print(selection_sort(nums))
