a31115e71561d7ef053a3523f9a6d8c542b9c235
bernylinville <bernylinville@devopsthink.org>
2021-03-07 12:38:56 +0800

    gift

--- learn/gift/common/utils.py
+++ learn/gift/common/utils.py
@@ -1,1 +1,13 @@
 # coding:utf-8
+import os
+
+from .error import NotFileError, NotPathError, FormatError
+
+
+def check_file(path):
+    if not os.path.exists(path):
+        raise NotPathError('don\'t found %s' % path)
+    if not path.endswith('.json'):
+        raise FormatError('need json format')
+    if not os.path.isfile(path):
+        raise NotFileError('this is not a file')
