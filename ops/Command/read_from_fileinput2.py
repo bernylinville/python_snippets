047f626329fa532d093e1c29030358d0b480936c
bernylinville <bernylinville@devopsthink.org>
2021-03-09 17:58:01 +0800

    fileinput

---
+++ ops/Command/read_from_fileinput2.py
@@ -0,0 +1,8 @@
+import fileinput
+
+for line in fileinput.input():
+    meta = [fileinput.filename(), fileinput.fileno(), fileinput.filelineno(),
+            fileinput.isfirstline(), fileinput.isstdin()]
+
+    print(*meta, end="\t")
+    print(line, end="")
