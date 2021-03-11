6ed47afa48d592b599bfe3bda24efd361b117bbe
bernylinville <bernylinville@devopsthink.org>
2021-03-11 11:07:07 +0800

    basicConfig_logging.py

---
+++ ops/Command/basicConfig_logging.py
@@ -0,0 +1,9 @@
+import logging
+
+logging.basicConfig(filename='app.log', level=logging.INFO)
+
+logging.debug('debug message')
+logging.info('info message')
+logging.warning('warning message')
+logging.error('error message')
+logging.critical('critical message')
